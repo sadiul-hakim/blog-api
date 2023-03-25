5e1820874672b53a1be90ed2202fc9b5b035980b
sadiul-hakim <sadiulhakim@gmail.com>
2023-03-25 23:06:16 +0600

    ROLE based auth

--- src/main/java/com/blog/controllers/UserController.java
+++ src/main/java/com/blog/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +38,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{userId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteUser(@PathVariable("userId") Integer userId){
             userService.deleteUser(userId);
             return ResponseEntity.ok(new ApiResponse("User deleted successfully",true));
--- src/main/java/com/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/blog/exceptions/GlobalExceptionHandler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -69,6 +70,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> disabledExceptionHandler(DisabledException ex){
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse(ex.getMessage(),false));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> accessDeniedExceptionHandler(AccessDeniedException ex){
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
                 .body(new ApiResponse(ex.getMessage(),false));
     }
 
